77a151fc944e81579d998d0c14f0aa0beb4a8180
Ricardo A. Interiano (Raim) <Raimmaster@users.noreply.github.com>
2016-09-16 01:52:28 -0600

    Instruction memory with loops

--- MIPS_Processor/InstructionMemory.v
+++ MIPS_Processor/InstructionMemory.v
@@ -6,10 +6,16 @@ module InstructionMemory(
     always @ (address)
     begin
         case (address)
-				8'h00: data = 16'h485A;
-            8'h01: data = 16'h4A14;
-            8'h02: data = 16'h4DF6;
+			8'h00: data = 16'h4864;
+            8'h01: data = 16'h4A0A;
+            8'h02: data = 16'h4C50;
             8'h03: data = 16'h4F96;
+            8'h04: data = 16'h0880;
+            8'h05: data = 16'h5614;
+            8'h06: data = 16'h6180;
+            8'h07: data = 16'h6848;
+            8'h08: data = 16'h8820;
+            8'h09: data = 16'h4F94;
             default: data = 16'h0000;
         endcase
     end
