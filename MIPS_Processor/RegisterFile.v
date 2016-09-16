56887f568425599d7b9e13b2bac9d53e2f856ad0
rNexeR <rnexer@gmail.com>
2016-09-15 21:18:14 -0600

    RegisterFiles get data without connect to Alu or other component

--- MIPS_Processor/RegisterFile.v
+++ MIPS_Processor/RegisterFile.v
@@ -1,18 +1,21 @@
 module RegisterFile(
 	input [1:0] read_addr1,
 	input [1:0] read_addr2,
+	input [1:0] read_addr_offline,
 	input [1:0] write_addr,
 	input [7:0] write_data,
 	input clk,
 	input write_enable,
 	output [7:0] read_data1,
-	output [7:0] read_data2
+	output [7:0] read_data2,
+	output [7:0] read_data_offline
 	);
 
 	reg [7:0] registers[0:3];
 
 	assign read_data1 = registers[read_addr1];
 	assign read_data2 = registers[read_addr2];
+	assign read_data_offline = registers[read_addr_offline];
 
 	always @ (posedge clk)
 	begin
