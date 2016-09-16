591fbadb6490e0b0727186740f77526e09aa59be
Ricardo A. Interiano (Raim) <Raimmaster@users.noreply.github.com>
2016-09-16 01:45:58 -0600

    Proc working at this point with adds

--- MIPS_Processor/InstructionMemory.v
+++ MIPS_Processor/InstructionMemory.v
@@ -6,11 +6,10 @@ module InstructionMemory(
     always @ (address)
     begin
         case (address)
-            8'h00: data = 16'h485A;
+				8'h00: data = 16'h485A;
             8'h01: data = 16'h4A14;
             8'h02: data = 16'h4DF6;
-				8'h03: data = 16'h4F96;
-				8'h04: data = 16'b0000100010000000;
+            8'h03: data = 16'h4F96;
             default: data = 16'h0000;
         endcase
     end
